20a1b857409ce94d3b84131778eb2dc2aa17cc37
Valeriy Kutenko <koot.n.co@gmail.com>
2022-12-27 15:44:28 +0200

    added all files

---
+++ src/main/java/com/hillel/lessons/lesson2/Main.java
@@ -0,0 +1,18 @@
+package com.hillel.lessons.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double x1 = 46.479395;
+        double y1 = 30.750729;
+        char degr = '\u00B0';
+        System.out.println("Памятник Тарасу Шевченко");
+        System.out.println("North: ");
+        System.out.print(x1);
+        System.out.print(degr);
+        System.out.println();
+        System.out.println("East");
+        System.out.print(y1);
+        System.out.print(degr);
+    }
+}
